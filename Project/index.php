4d586e40422e2d5c2bbd086ac522f880c9298288
isrt09 <mazedur@gmail.com>
2020-12-08 22:52:58 +0600

    Insert operation is done

--- Project/index.php
+++ Project/index.php
@@ -2,21 +2,49 @@
     require_once('./includes/db.php');
     require_once('./includes/header.php'); 
 ?>
-    <div class="container">
-        <form class="py-4">
+ <?php 
+    if(isset($_POST['submit']))
+    {        
+        $message       = '';
+        $user_name     = $_POST['user_name'];
+        $user_email    = $_POST['user_email'];                                
+        $user_password = 'SECRET';                                
+        if(empty($user_name) || empty($user_email))
+        {
+            $error = true;
+        }else{
+            $message = '';
+            $sql  = "INSERT INTO users(user_name,user_email,user_password) VALUES(:user_name,:user_email,:user_password)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
+                ':user_name'     => $user_name,
+                ':user_email'    => $user_email,
+                ':user_password' => $user_password
+            ]);            
+        }
+    }
+?>
+    <div class="container">        
+        <form class="py-4" method="POST" action="index.php"> 
+        <?php if(isset($message)){ echo "<div class='alert alert-success'>Data Inserted Successfully!</div>";} ?>           
             <div class="row">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Username">
+                    <input type="text" name="user_name" class="form-control" placeholder="Username">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Email Address">
+                    <input type="email" name="user_email" class="form-control" placeholder="Email Address">
                 </div>
                 <div class="col">
-                    <input type="submit" class="form-control btn btn-secondary" value="Add New User">
+                    <input type="submit" name="submit" class="form-control btn btn-secondary" value="Add New User">
                 </div>
-            </div>
+                <?php 
+                    if(isset($error)){
+                        echo "<script>alert('Field can not be empty or blank...')</script>";
+                    }
+                 ?>          
+            </div>                
         </form>
-
+        
         <h2>All Users</h2>
         <table class="table">
             <thead class="thead-dark">
