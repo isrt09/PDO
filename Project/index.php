b019afd267d5405b3d25c9ff9dd128533077a7bf
isrt09 <mazedur@gmail.com>
2020-12-08 19:05:25 +0600

    database connection & configuration

--- Project/index.php
+++ Project/index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Task List</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
+<?php 
+    require_once('./includes/db.php');
+    require_once('./includes/header.php'); 
+?>
     <div class="container">
         <form class="py-4">
             <div class="row">
@@ -53,5 +47,4 @@
         </table>
 
     </div>
-</body>
-</html>
+<?php require_once('./includes/footer.php'); ?>
